3a72fd3056738308d9218450780e00376798bd8d
Nick Nguyen <nickmn2006@gmail.com>
2024-09-26 15:04:48 -0700

    setup started 🤫🧏‍♂️

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
 from constants import *
+    # print("Starting asteroids!")
+    # print(f'Screen width: {SCREEN_WIDTH}')
+    # print(f'Screen height: {SCREEN_HEIGHT}')
 
 def main():
-    print("Starting asteroids!")
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
-
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        pass
 
 if __name__ == '__main__':
     main()
+
+
