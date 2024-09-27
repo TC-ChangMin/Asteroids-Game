e1b2c5df9a4e9ba9643c783b2d7f3eae49ba4054
Nick Nguyen <nickmn2006@gmail.com>
2024-09-26 20:05:23 -0700

    screen setup completed

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 if __name__ == '__main__':
     main()
