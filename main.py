2f25501ee5b68f76959210e35741094adc4b36b1
Nick Nguyen <nickmn2006@gmail.com>
2024-09-26 21:11:15 -0700

    added player. code broken

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import pygame
 from constants import *
-    # print("Starting asteroids!")
-    # print(f'Screen width: {SCREEN_WIDTH}')
-    # print(f'Screen height: {SCREEN_HEIGHT}')
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x,y)
+    dt = 0
+
+    Player(x,y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+        screen.fill('black')
         pygame.display.flip()
+        player.draw(screen)
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == '__main__':
     main()
