d086e26f9dd77aa03be157a82b320c1f737a3518
Nick Nguyen <nickmn2006@gmail.com>
2024-09-26 21:17:56 -0700

    order of operations issue, code worked you just didnt draw the player before updating screen

--- main.py
+++ main.py
@@ -6,19 +6,17 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x,y)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
-    Player(x,y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
-        pygame.display.flip()
         player.draw(screen)
+        pygame.display.flip()
 
         dt = clock.tick(60) / 1000
 
