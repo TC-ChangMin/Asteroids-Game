1deda230b5098eea789bdb5f9634686bc0494fb4
Nick Nguyen <nickmn2006@gmail.com>
2024-09-28 11:52:34 -0700

    starting asteroids

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -14,8 +19,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        for object in updatable:
+            object.update(dt)
+
         screen.fill('black')
-        player.draw(screen)
+
+        for object in drawable:
+            object.draw(screen)
+            
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
